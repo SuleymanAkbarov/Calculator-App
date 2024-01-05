71d4326f03687524b1470f8e75f27b8d5c7e7a84
Suleyman Akbarov <akberovsuleyman@gmail.com>
2024-01-05 23:21:03 +0400

    first commit

---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.calc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
